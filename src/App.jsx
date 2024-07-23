684a7cef061eaab23e2eb63e0b4540ca6ffe0893
Tony Le <15260999-TonyLe21@users.noreply.replit.com>
2024-07-23 20:17:47 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './App.css';
+import Header from './Header';
+import Employee from './Employee';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Employee/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
